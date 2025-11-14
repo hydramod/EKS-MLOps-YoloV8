caf39851c8fb59378bb7eaee1ef38d1be2b8e24c
hydramod <alissaeid@live.co.uk>
2025-11-14 21:59:16 +0400

    add version

---
+++ infra/modules/helm/versions.tf
@@ -0,0 +1,39 @@
+terraform {
+
+  required_providers {
+
+    kubectl = {
+
+      source  = "alekc/kubectl"
+
+      version = "~> 2.0"
+
+    }
+
+    kubernetes = {
+
+      source  = "hashicorp/kubernetes"
+
+      version = "~> 2.23"
+
+    }
+
+    helm = {
+
+      source  = "hashicorp/helm"
+
+      version = "~> 2.11"
+
+    }
+
+    aws = {
+
+      source  = "hashicorp/aws"
+
+      version = "~> 5.0"
+
+    }
+
+  }
+
+}
